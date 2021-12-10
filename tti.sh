380b8c06bec589c9729c5e293d9c7f195b88e2a3
hydrastro <9256943+hydrastro@users.noreply.github.com>
2021-12-10 21:45:10 +0100

    Captcha ?

--- tti.sh
+++ tti.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 : "${WRAP_COLUMN:=80}"
-: "${FONT_FILE:=}"
-: "${FONT_SIZE:=20}"
-: "${FILL_COLOR:=white}"
-: "${BACKGROUND_COLOR:=black}"
+#: "${FONT_FILE:=/usr/share/fonts/truetype/unifont/unifont.ttf}"
+: "${FONT_SIZE:=25}"
+: "${FILL_COLOR:=black}"
+: "${BACKGROUND_COLOR:=white}"
+: "${NOISE_ROUNDS:=2}"
+: "${NOISE_TYPE:=Impulse}"
+: "${LINES_PER_10000PX:=3}"
+: "${LINES_THICNESS:=1}"
+: "${LINES_COLOR:=black}"
 
 #
 # Generate Image
@@ -13,10 +18,11 @@
 # $2 Filename
 #
 function tti_generate_image() {
-    local output_image imagemagick_options
+    local output_image imagemagick_options escaped i X_0 X_1 Y_0 Y_1
     if [[ $# -lt 1 ]]; then
         tti_exit "Error: missing argument(s) for ${FUNCNAME[0]}"
     fi
+    escaped=$(tti_adjust_text "$1")
     output_image="$2"
     imagemagick_options=(
         #-font "$FONT_FILE"
@@ -31,11 +37,65 @@ function tti_generate_image() {
         #-interword-spacing "$INTERWORD_SPACING"
         #-interline-spacing "$INTERLINE_SPACING"
         #-gravity "$GRAVITY"
-        label:"$1"
+        label:"$escaped"
+        #-emboss 0x1
         -flatten
-        "$output_image"
     )
-    convert "${imagemagick_options[@]}"
+    i=0
+    while [[ "$i" -lt "$NOISE_ROUNDS" ]]; do
+        imagemagick_options+=(+noise "$NOISE_TYPE")
+        ((i = i + 1))
+    done
+    convert "${imagemagick_options[@]}" "$output_image"
+    i=0
+    tti_get_image_size "$output_image"
+    lines_number=$((("$LINES_PER_10000PX" * "$WIDTH" * "$HEIGHT") / 10000))
+    echo $lines_number
+    imagemagick_second_round=(
+        "$output_image"
+        -flatten
+    )
+    while [[ "$i" -lt "$lines_number" ]]; do
+        X_0=$(tti_get_random_number "0" "$WIDTH")
+        X_1=$(tti_get_random_number "0" "$WIDTH")
+        Y_0=$(tti_get_random_number "0" "$HEIGHT")
+        Y_1=$(tti_get_random_number "0" "$HEIGHT")
+        imagemagick_second_round+=(-stroke "$LINES_COLOR" -strokewidth "$LINES_THICNESS")
+        imagemagick_second_round+=(-draw "line $X_0,$Y_0 $X_1,$Y_1")
+        ((i = i + 1))
+    done
+    imagemagick_second_round+=(-swirl 45)
+    convert "${imagemagick_second_round[@]}" "$output_image"
+}
+
+
+#
+# Get Image Size
+#
+# $1 Filename
+#
+function tti_get_image_size() {
+    if [[ $# -eq 0 ]]; then
+        tti_exit "Error: missing argument(s) for ${FUNCNAME[0]}"
+    fi
+    if [[ ! -f "$1" ]]; then
+        tti_exit "Error: image not found"
+    fi
+    WIDTH=$(identify -format '%w' "$1")
+    HEIGHT=$(identify -format '%h' "$1")
+}
+
+#
+# Get Random Number
+#
+# $1 Min
+# $2 Max
+#
+function tti_get_random_number() {
+    if [[ $# -le 1 ]]; then
+        tti_exit "Error: missing argument(s) for ${FUNCNAME[0]}"
+    fi
+    shuf -i "$1"-"$2" -n 1
 }
 
 #
@@ -43,11 +103,11 @@ function tti_generate_image() {
 #
 # $1 Text
 #
-function tti_wrap_text() {
+function tti_adjust_text() {
     if [[ $# -eq 0 ]]; then
         tti_exit "Error: missing argument(s) for ${FUNCNAME[0]}"
     fi
-    printf "%s" "$1" | fold -w 80
+    printf "%s" "$1" | sed -E 's/(\\|@|%)/\\\1/g' | fold -w 24
 }
 
 #
@@ -82,25 +142,23 @@ function tti_exit() {
 # $1 Text
 #
 function tti_main() {
-    local buffer previous_line output_image
+    local buffer output_image
     buffer=""
-    previous_line=""
     while IFS= read -r line; do
         if [[ "$buffer" != "" ]]; then
             buffer+=$'\n'
         fi
-        buffer+="$line"
-        if [[ "$line" == "" && "$previous_line" == "." ]]; then
-            buffer=$(printf "%s" "$buffer" | head -n -1)
+        if [[ "$line" == "." ]]; then
             break
         fi
-        previous_line="$line"
+        buffer+="$line"
     done
     output_image="/tmp/$(date +%s%N | cut -b1-13).jpg"
     tti_generate_image "$buffer" "$output_image"
     tti_copy_to_clipboard "$output_image"
     if [[ -f "$output_image" ]]; then
-        rm "$output_image"
+       # rm "$output_image"
+       echo "OK"
     fi
 }
 
